da74d68be7907e3f4c1d39f4f28411e2b5a1dff2
luidimso <luidi_mso@outlook.com>
2023-11-25 16:21:24 -0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.telegram.org/bot6059184035:AAHByKr2MUZSW6iNSMBcDPibICMztWLkAGU/sendMessage",
+    "method": "POST",
+    "headers": {
+      "Content-Type": "application/json",
+      "cache-control": "no-cache"
+    },
+    "data": JSON.stringify({
+      "chat_id": "-4050432711",
+      "text": "Olha só"
+    })
+  }
+
 function sendText() {
-    postText("olhasoquemvemaiocara", "test");
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    }); 
 }
